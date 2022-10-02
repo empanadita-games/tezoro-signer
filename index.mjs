a98d959c9b5594092c11796da9f14261f38d3a7f
tezosmiami <tezosmiami@gmail.com>
2022-10-02 16:38:25 -0400

    final for now. . .

--- index.mjs
+++ index.mjs
@@ -5,7 +5,7 @@ import cors from "cors";
 import "dotenv/config";
 
 
-const Tezos = new TezosToolkit('https://kathmandunet.tezos.marigold.dev/');
+const Tezos = new TezosToolkit('https://ghostnet.tezos.marigold.dev/');
 Tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(process.env.SIGNING_KEY) });
 
 const port =  process.env.PORT || 3000;
@@ -16,10 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.post('/sendTez', (req, res) => {
-
+  
     let data = req.body 
   
-    if (data.amount == null || data.address == null) {
+    if (data.amount == null || data.address.length != 36) {
         return res.status(400).send("Invalid request: missing params.");
       }
 
@@ -35,31 +35,29 @@ app.post('/sendTez', (req, res) => {
 
 app.post('/sendObjkt', (req, res) => {
     
-    let data = req.body 
-  
-    if (data.address == null || data.token_id == null) {
-        return res.status(400).send("Invalid request: missing params.");
-      }
-     
-    const params = [{from_: 'tz1XRPyYPj85qUmY9uHRp6JeAHBrKuLvLUni', txs: [{to_: data.address, token_id: data.token_id, amount: 1}]}]
-    console.log(`Transfering Objkt to ${data.to_}...`);
-    Tezos.contract
-    .at('KT18shTEJc8wGGtyuVmLcBvmU2rUYCRLeCqe')
-    .then((c) => {
-      let transfer = c.methods.transfer(params).send({
-       amount: 0,
-       mutez: true,
-       storageLimit: 310
-   })
-   .then((op) => {
-     console.log(`Waiting for ${op.hash} to be confirmed...`);
-     return op.confirmation(1).then(() => op.hash);
-   })
-   .then((hash) => console.log(`Operation injected: https://kathmandu.tzstats.com/${hash}`))
-   .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`));
+     let data = req.body 
+   
+     if (data.address.length != 36 || data.token_id == null) {
+         return res.status(400).send("Invalid request: missing params.");
+       }
+     const params = [{from_: 'tz1XRPyYPj85qUmY9uHRp6JeAHBrKuLvLUni', txs: [{to_: data.address, token_id: data.token_id, amount: 1}]}]
+     console.log(`Transfering Objkt to ${data.address}...`);
+     Tezos.contract
+     .at('KT18shTEJc8wGGtyuVmLcBvmU2rUYCRLeCqe')
+     .then((c) => {
+       c.methods.transfer(params).send({
+        amount: 0,
+        mutez: true,
+        storageLimit: 310
     })
-});
-
+    .then((op) => {
+      console.log(`Waiting for ${op.hash} to be confirmed...`);
+      return op.confirmation(1).then(() => op.hash);
+    })
+    .then((hash) => console.log(`Operation injected: https://ghost.tzstats.com/${hash}`))
+    .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`));
+     })
+ });
 
 app.listen(port, function(err){
     if (err) console.log(err);
